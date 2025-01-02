a7db348b8d188f8ec3e068f4c00a153a8b2c8aa4
Rick Hanlon <rickhanlonii@fb.com>
2025-01-02 14:26:39 -0500

    Example: passing promise from server to client

--- app/r19-client-side-and-server-function/page.js
+++ app/r19-client-side-and-server-function/page.js
@@ -1,47 +1,13 @@
-"use client";
+import { Suspense } from "react";
+import { fetchUsers } from "./actions";
+import Users from './users';
 
-/* has error:
-Cannot update a component (`Router`) while rendering a different component (`App`).
-To locate the bad setState() call inside `App`
-*/
 
-import { use, Suspense } from "react";
-import { fetchUsers } from "./actions"; // Server Function that returns a promise
 
-let cachedPromise;
-
-function getCachedUserPromise() {
-  if (!cachedPromise) {
-    cachedPromise = fetchUsers();
-  }
-  return cachedPromise;
-}
-
-const Users = function ({userPromise}) {
-
-  const users = use(userPromise);
-
-  return (
-    <div>
-      <h1>Client Side Only (use hook and suspense)</h1>
-      <ul>
-        {users.map((user) => (
-          <div key={user.id}>
-            <h2>{user.name}</h2>
-          </div>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-function App() {
-
-  const userPromise = getCachedUserPromise();
-
+async function App() {
   return (
     <Suspense fallback={<h1>Loading...</h1>}>
-      <Users userPromise={userPromise} />
+      <Users userPromise={fetchUsers()} />
     </Suspense>
   );
 }
