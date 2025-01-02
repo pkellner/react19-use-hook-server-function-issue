05e8fcd271889ec28fb148bc979dbef7c277cce1
pkellner <peter@peterkellner.net>
2025-01-02 11:42:54 -0800

    added promise caching for re-renders

--- app/r19-client-side-and-server-function/page.js
+++ app/r19-client-side-and-server-function/page.js
@@ -2,12 +2,19 @@ import { Suspense } from "react";
 import { fetchUsers } from "./actions";
 import Users from './users';
 
+let cachedUserPromise;
 
+function getCachedUserPromise() {
+  if (!cachedUserPromise) {
+    cachedUserPromise = fetchUsers();
+  }
+  return cachedUserPromise;
+}
 
 async function App() {
   return (
     <Suspense fallback={<h1>Loading...</h1>}>
-      <Users userPromise={fetchUsers()} />
+      <Users userPromise={getCachedUserPromise()} />
     </Suspense>
   );
 }
